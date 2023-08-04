67c67bc4324b4031c5cd19857977493ddb4e9bd0
Mehdi465 <mehdi.fellahi06@gmail.com>
2023-08-05 00:46:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# web_server_for_learning_words
---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request
+import json
+
+app = Flask(__name__)
+
+
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    dict_ = {}
+    dict_[request.args.to_dict()["word"]]=request.args.to_dict()["value"]
+
+    try:
+        with open("json_file/DB.json", "r") as json_file:
+            existing_data = json.load(json_file)
+
+    except FileNotFoundError:
+    # If the file doesn't exist yet, start with an empty list
+        existing_data = []
+
+    # Step 2: Convert the JSON data to a Python object (list of dictionaries)
+    print(existing_data)
+    # Step 3: Append your new dictionary to the Python object
+    existing_data.update(dict_)
+
+    # Step 4: Write the updated Python object back to the JSON file
+    with open("json_file/DB.json", "w") as json_file:
+        json.dump(existing_data, json_file, indent=4)
+
+    return render_template("form.html")
+
+
+@app.route("/exam")
+def exam():
+    with open("json_file/DB.json","r+") as json_file:
+        dict_ = json.load(json_file)
+        print(dict_)
+    return render_template("transaction.html",entries=dict_)
+
