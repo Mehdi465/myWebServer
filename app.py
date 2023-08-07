23afc9646c6d2a455840428a2b05cf94c5054429
Mehdi465 <mehdi.fellahi06@gmail.com>
2023-08-07 22:16:04 +0200

    changing everything

--- app.py
+++ app.py
@@ -3,50 +3,22 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/")
+@app.route("/home")
 def home():
-    dict_ = {}
-    print("OUEEEEEEEEEEEEEE" + request.url)
+    return render_template("home.html")
 
 
-    #print("origine : " + str(request.headers))
-
-    if len(request.args) != 0:
-        dict_[request.args.to_dict()["word"]]=request.args.to_dict()["value"]
-
-    try:
-        with open("json_file/DB.json", "r") as json_file:
-            existing_data = json.load(json_file)
-
-    except FileNotFoundError:
-    # If the file doesn't exist yet, start with an empty list
-        existing_data = []
-
-    # Step 2: Convert the JSON data to a Python object (list of dictionaries)
-    # Step 3: Append your new dictionary to the Python object
-    existing_data.update(dict_)
-
-    # Step 4: Write the updated Python object back to the JSON file
-    with open("json_file/DB.json", "w") as json_file:
-        json.dump(existing_data, json_file, indent=4)
-
-    return render_template("form.html")
-
+""" 
 
 @app.route("/exam")
 def exam():
     with open("json_file/DB.json","r+") as json_file:
         dict_ = json.load(json_file)
         
-    return render_template("transaction.html",entries=dict_)
+    return render_template("transaction.html",entries=list(dict_.keys()))
 
-
-@app.route("/exam",methods=["GET","POST"])
-def get_result():
-    with open("json_file/DB.json","r+") as json_file:
-        dict_ = json.load(json_file)
-
-    return render_template("transaction_results.html",entries=list(dict_.values))
+ """
 
 
 
